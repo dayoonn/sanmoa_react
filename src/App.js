95f8bf6f76abdbcf12686a39c3e1f0a37462d4e9
dayoon <dayo0319@naver.com>
2022-09-12 22:16:22 +0900

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Main from './Main';
+import React, {useState, useEffect} from 'react';
+import './css/style.css';
+import MainSearch from "./MainSearch";
+import Map from "./Map";
+import Emergency from "./Emergency";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const API_END_POINT = process.env.REACT_APP_API_ENDPOINT;
+
+const App = () => {
+   /* const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get(`${API_END_POINT}/store/cafe`).then((res) => {
+            setData(
+                res.data.elements[0].elements[1].elements[0].elements[0].elements[0]
+                    .elements[0].text
+            );
+        });
+    }, []);
+    console.log(data); */
+   // axios.post(`${API_END_POINT}/sanmoa/route`, {params:{name:'dayooneee' }});
+
+    return (
+        <div>
+            <Map/>
+        </div>
+    );
+};
 export default App;
