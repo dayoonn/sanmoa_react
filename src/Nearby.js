a7f8e5411e38c7d9611092fd7e9f891b6acece5e
GabeenKim <rlarkqls5551250@gmail.com>
2022-09-19 05:00:04 +0900

    feat:store_search_

--- src/Nearby.js
+++ src/Nearby.js
@@ -3,122 +3,102 @@ import axios from 'axios';
 import './css/style.css';
 import { Link } from 'react-router-dom';
 import 'antd/dist/antd.css';
+import { Row, Col, Menu } from 'antd';
+import { MenuFoldOutlined, MenuOutlined } from '@ant-design/icons';
+import StoreCard from './Component/StoreSearch';
 import StoreSearchContainer from './Container/StoreSearchContainer';
 
 const API_END_POINT = process.env.REACT_APP_API_ENDPOINT;
 
 const Nearby = () => {
   const [store, setStore] = useState([]);
-
+  const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get(`${API_END_POINT}/store/cafe`).then((response) => {
+    axios.get(`${API_END_POINT}/store/restaurant`).then((response) => {
       if (response.data) {
         setStore(response.data.response.body.items.item);
+        setData(response.data.response.body.items.item);
       } else {
         alert('리스트를 가져오는데 실패했습니다.');
       }
     });
   }, []);
 
-  console.log(store);
+  /**추가**/
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleBar, setToggleBar] = useState(true);
+
+  const toggleChange = () => {
+    setToggleMenu(!toggleMenu);
+    setToggleBar(!toggleBar);
+  };
+
+  const onMenuClick = () => {
+    setToggleMenu(!toggleMenu);
+    setToggleBar(!toggleBar);
+  };
+  /**추가**/
 
   return (
     <div>
-      <div>
+      <nav>
         <header id="main_header">
           <h1>SANMOA</h1>
         </header>
+
         <header id="menu_header">
-          <h2>맛집</h2>
+          <div className="menu_inline">
+            <button id="menu" type="primary" onClick={toggleChange}>
+              {toggleBar ? <MenuOutlined /> : <MenuFoldOutlined />}
+            </button>
+
+            <div className="menu_title">
+              {' '}
+              <h2>맛집 검색</h2>
+            </div>
+          </div>
         </header>
+      </nav>
+
+      <div className="nav_bar">
+        {toggleMenu && (
+          <Menu
+            defaultSelectedKeys={['1']}
+            mode="inline"
+            theme="light"
+            inlineCollapsed={toggleBar}
+            onClick={onMenuClick}
+          >
+            <Menu.Item>
+              <Link to="/">등산로 검색</Link>
+            </Menu.Item>
+
+            <Menu.Item>
+              <Link to="/community">커뮤니티</Link>
+            </Menu.Item>
+
+            <Menu.Item>
+              <Link to="/mypage">마이페이지</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/emergency">응급처치</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/Nearby">맛집 검색</Link>
+            </Menu.Item>
+          </Menu>
+        )}
       </div>
       <div>
         <StoreSearchContainer item={store} />
       </div>
-
-      {/* <ul className="img-box">
+      <Row>
         {data.map((data) => (
-          <li
-            key={data.id}
-            className="row align-items-center m-0"
-            style={{ float: 'left', margin: '15px' }}
-          >
-            <div className="col-1 py-2">
-              <img
-                src={data.firstimage}
-                alt={data.title}
-                style={{ width: '180px', height: '120px' }}
-                className="img-fluid rounded-circle"
-              />
-            </div>
-            <div className="content">
-              <span className="col" style={{ fontSize: '15px' }}>
-                {data.title}
-              </span>
-              <br />
-              <span id="other" style={{ fontSize: '10px' }}>
-                {data.tel}
-              </span>
-              <br />
-              <span id="other" style={{ fontSize: '10px' }}>
-                {data.addr1}
-              </span>
-            </div>
-          </li>
+          <Col xs={24} sm={12} md={6} lg={4} xl={4}>
+            <StoreCard item={data}></StoreCard>
+          </Col>
         ))}
-      </ul> */}
-
-      <div id="footer">
-        <nav id="footer_gnb">
-          <div>
-            <Link to="/">
-              <div className="icon_div">
-                <img src={require('./img/search.png')} className="menu_icon" />
-              </div>
-              검색
-            </Link>
-          </div>
-
-          <div>
-            <Link to="/">
-              <div className="icon_div">
-                <img
-                  src={require('./img/community.png')}
-                  className="menu_icon"
-                />
-              </div>
-              커뮤니티
-            </Link>
-          </div>
-
-          <div>
-            <Link to="/mypage">
-              <div className="icon_div">
-                <img src={require('./img/mypage.png')} className="menu_icon" />
-              </div>
-              마이페이지
-            </Link>
-          </div>
-
-          <div>
-            <Link to="/emergency">
-              <div className="icon_div">
-                <img src={require('./img/aid.png')} className="menu_icon" />
-              </div>
-              응급처치
-            </Link>
-          </div>
-
-          <div>
-            <Link to="/Nearby">
-              <div className="icon_div">
-                <img src={require('./img/matzip.png')} className="menu_icon" />
-              </div>
-              맛집
-            </Link>
-          </div>
-        </nav>
-      </div>
+      </Row>
     </div>
   );
 };
