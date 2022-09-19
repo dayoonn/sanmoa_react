6135119f4049bb0f90b2c69cbdaf2a227adf37cd
GabeenKim <rlarkqls5551250@gmail.com>
2022-09-19 19:11:23 +0900

    feat:게시판list

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import './App.css';
-import axios from 'axios';
 import Main from './Main';
 import React, { useState, useEffect } from 'react';
 import './css/style.css';
 import MainSearch from './MainSearch';
-import Map from './Map';
-import Emergency from './Emergency';
 import MyPage_co from './MyPage_co';
 import MyPage from './MyPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './signup';
 import Login from './login';
 import Nearby from './Nearby';
-import Commu from './Commu';
-import Login_welcome from './login_welcome';
 import 'antd/dist/antd.css';
+import PostMain from './PostMain';
+import PostView from './PostView';
 
 const API_END_POINT = process.env.REACT_APP_API_ENDPOINT;
 
 const App = () => {
-  /* const [data, setData] = useState(null);
-     useEffect(() => {
-         axios.get(`${API_END_POINT}/store/cafe`).then((res) => {
-             setData(
-                 res.data.elements[0].elements[1].elements[0].elements[0].elements[0]
-                     .elements[0].text
-             );
-         });
-     }, []);
-     console.log(data); */
-  // axios.post(`${API_END_POINT}/sanmoa/route`, {params:{name:'dayooneee' }});
-
   return (
     <>
       <Router>
@@ -38,9 +23,9 @@ const App = () => {
           <Route path="/" exact={true} element={<MainSearch />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/mypage_co" element={<MyPage_co />} />
-          <Route path="/emergency" element={<Emergency />} />
-          <Route path="/community" element={<Commu />} />
           <Route path="/mapsearch" element={<Main />} />
+          <Route path="/postView/:id" element={<PostView />} />
+          <Route path="/community" element={<PostMain />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/Nearby" element={<Nearby />} />
---
+++ src/PostList.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './css/style.css';
+import { Link } from 'react-router-dom';
+import CommonTable from './table/CommonTable';
+import CommonTableColumn from './table/CommonTableColumn';
+import CommonTableRow from './table/CommonTableRow';
+import { BugOutlined, MenuFoldOutlined, MenuOutlined } from '@ant-design/icons';
+import { Button, Menu } from 'antd';
+
+const API_END_POINT = process.env.REACT_APP_API_ENDPOINT;
+
+const PostList = (props) => {
+  const [postlist, setPostList] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API_END_POINT}/board`).then((response) => {
+      console.log(response.data);
+      setPostList(response.data.data);
+    });
+  }, []);
+
+  /**추가**/
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleBar, setToggleBar] = useState(true);
+
+  const toggleChange = () => {
+    setToggleMenu(!toggleMenu);
+    setToggleBar(!toggleBar);
+  };
+
+  const onMenuClick = () => {
+    setToggleMenu(!toggleMenu);
+    setToggleBar(!toggleBar);
+  };
+  /**추가**/
+
+  return (
+    <div>
+      <nav>
+        <header id="main_header">
+          <h1>SANMOA</h1>
+        </header>
+
+        <header id="menu_header">
+          <div className="menu_inline">
+            <button id="menu" type="primary" onClick={toggleChange}>
+              {toggleBar ? <MenuOutlined /> : <MenuFoldOutlined />}
+            </button>
+
+            <div className="menu_title">
+              <h2>커뮤니티 </h2>
+            </div>
+          </div>
+        </header>
+      </nav>
+      <div className="nav_bar">
+        {toggleMenu && (
+          <Menu
+            defaultSelectedKeys={['1']}
+            mode="inline"
+            theme="light"
+            inlineCollapsed={toggleBar}
+            onClick={onMenuClick}
+          >
+            <Menu.Item>
+              <Link to="/">등산로 검색</Link>
+            </Menu.Item>
+
+            <Menu.Item>
+              <Link to="/community">커뮤니티</Link>
+            </Menu.Item>
+
+            <Menu.Item>
+              <Link to="/mypage">마이페이지</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/emergency">응급처치</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/Nearby">맛집 검색</Link>
+            </Menu.Item>
+          </Menu>
+        )}
+      </div>
+      <CommonTable headersName={['글번호', '제목', '등록일']}>
+        {postlist &&
+          postlist.map((item, index) => {
+            let dateObj = new Date(item.postdate);
+            let timeString_KR = dateObj.toLocaleString('ko-KR', {
+              timeZone: 'Asia/Seoul',
+            });
+            return (
+              <CommonTableRow key={index}>
+                <CommonTableColumn>{index + 1}</CommonTableColumn>
+                <Link to={`/postView/${item.id}`}>{item.title}</Link>
+                <CommonTableColumn>{timeString_KR}</CommonTableColumn>
+              </CommonTableRow>
+            );
+          })}
+      </CommonTable>
+    </div>
+  );
+};
+
+export default PostList;
