7e06cd98cc4df3cfde3915ed9746e6c2a01462c1
GabeenKim <rlarkqls5551250@gmail.com>
2022-10-14 08:49:12 +0900

    mypage 다시 수정

--- src/MyPage.js
+++ src/MyPage.js
@@ -8,6 +8,7 @@ import { Menu, Button } from 'antd';
 const API_END_POINT = process.env.REACT_APP_API_ENDPOINT;
 
 const MyPage = () => {
+  const [name, setName] = useState('');
   const [age, setAge] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -23,6 +24,7 @@ const MyPage = () => {
         },
       })
       .then(function (res) {
+        setName(res.data.userInfo.name);
         setAge(res.data.userInfo.age);
         setEmail(res.data.userInfo.email);
         setPhone(res.data.userInfo.phone);
@@ -109,6 +111,7 @@ const MyPage = () => {
       <div id="main_box_mypage">
         <div className="my_div">
           <div className="title"> 내 정보</div>
+          <div id="my_name">이름 : {name}</div>
           <div id="my_mail">email : {email}</div>
           <div id="my_age">나이 : {age}</div>
           <div id="my_phone">연락처 : {phone}</div>
